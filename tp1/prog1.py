e852d62ec5b41923542e81281f4d2a0abd35e385
lroyer0 <lroyer0@icloud.com>
2025-09-02 17:02:36 +0200

    ajout tests unitaires

--- tp1/prog1.py
+++ tp1/prog1.py
@@ -1,1 +1,14 @@
-print("Hello, World! c'est léo !")
+import fonctions as f
+
+print("Hello, World! c'est léo")
+
+while True:
+    try:
+        nombre = float(input("Entrez un nombre : "))
+        carre = nombre ** 2
+        print(f"Le carré de {nombre} est {carre}")
+    except KeyboardInterrupt:
+        print("\nArrêt du programme")
+        break
+    except ValueError:
+        print("Veuillez entrer un nombre valide")
